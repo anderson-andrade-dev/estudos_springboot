95545db761a6b9311f8b45c7537189ffb391d8a9
Anderson Andrade <147995158+anderson-andrade-dev@users.noreply.github.com>
2024-06-18 01:10:58 -0300

    primeiro programa do livro Spring boot

---
+++ src/test/java/br/com/andersonandradedev/estudosSpringBoot3/EstudosSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.andersonandradedev.estudosSpringBoot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosSpringBoot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
